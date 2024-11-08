1cb285c773e96530e41ed24caa9690175c741b47
yuwantha98 <yuwantharavihara123@gmail.com>
2024-11-08 17:18:58 +0530

    change first navigation to login page

--- Car-Rental-Service-Spring-Angular-NGZorro/Angular/src/app/app-routing.module.ts
+++ Car-Rental-Service-Spring-Angular-NGZorro/Angular/src/app/app-routing.module.ts
@@ -1,14 +1,33 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { SignupComponent } from './auth/components/signup/signup.component';
+// import { LoginComponent } from './auth/components/login/login.component';
+//
+// const routes: Routes = [
+//   { path: "register", component: SignupComponent },
+//   { path: "login", component: LoginComponent },
+//   { path: "admin", loadChildren: () => import("./modules/admin/admin.module").then(m => m.AdminModule) },
+//   { path: "customer", loadChildren: () => import("./modules/customer/customer.module").then(m => m.CustomerModule) }
+//
+// ];
+//
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './auth/components/signup/signup.component';
 import { LoginComponent } from './auth/components/login/login.component';
 
 const routes: Routes = [
-  { path: "register", component: SignupComponent },
-  { path: "login", component: LoginComponent },
-  { path: "admin", loadChildren: () => import("./modules/admin/admin.module").then(m => m.AdminModule) },
-  { path: "customer", loadChildren: () => import("./modules/customer/customer.module").then(m => m.CustomerModule) }
-
+  { path: '', redirectTo: '/login', pathMatch: 'full' },  // Redirects root URL to login
+  { path: 'register', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) },
+  { path: 'customer', loadChildren: () => import('./modules/customer/customer.module').then(m => m.CustomerModule) }
 ];
 
 @NgModule({
@@ -16,3 +35,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
